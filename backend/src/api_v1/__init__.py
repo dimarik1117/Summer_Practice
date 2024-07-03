9f03dab8f3e7fbc10fb604283e53eed9e6c0cb79
Dmitriy <124895700+dimarik1117@users.noreply.github.com>
2024-07-03 20:19:16 +0300

    Delete backend

--- backend/src/api_v1/__init__.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-
-from .vacancies.views import  router as vacancie_router
-
-router = APIRouter()
-router.include_router(router=vacancie_router, prefix="/vacancies")
--- backend/src/core/models/base.py
+++
@@ -1,6 +0,0 @@
-from typing import List
-from sqlalchemy.orm import DeclarativeBase
-
-
-class Base(DeclarativeBase):
-    pass
